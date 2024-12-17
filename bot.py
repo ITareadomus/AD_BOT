55c8a3c73fd7f7f1a2f8924a3ce137dcbc86b918
Markk <marcoale.specchio@gmail.com>
2024-12-17 14:12:04 +0100

    tolto filters channel

--- bot.py
+++ bot.py
@@ -95,7 +95,7 @@ def main():
     application.add_handler(MessageHandler(filters.TEXT & ~filters.REPLY, handle_message))
 
     # Gestore delle risposte degli amministratori (anche per media)
-    application.add_handler(MessageHandler(filters.ALL & filters.REPLY & filters.CHANNEL, handle_response))
+    application.add_handler(MessageHandler(filters.TEXT & filters.REPLY, handle_response))
 
     # Gestore degli errori
     application.add_error_handler(error_handler)
@@ -104,4 +104,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
