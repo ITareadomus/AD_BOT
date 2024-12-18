21032679acb51f18e8d7867e688fd688f23897b8
Markk <marcoale.specchio@gmail.com>
2024-12-18 16:14:14 +0100

    oupl

--- bot.py
+++ bot.py
@@ -6,28 +6,22 @@ from telegram.ext import ApplicationBuilder, MessageHandler, filters, ContextTyp
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-# ID del canale in cui effettuare il controllo
-TARGET_CHANNEL_ID = -1002350584252
-
 # Funzione per gestire i messaggi di testo
 async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
     if update.message:
         logger.info(f"Messaggio ricevuto nella chat con ID: {update.message.chat.id}")
 
-        if update.message.chat.id == TARGET_CHANNEL_ID:  # Controlla se il messaggio è nel canale target
-            logger.info("Messaggio ricevuto dal canale target.")
-            
-            if not update.message.text:  # Controlla se il messaggio non è testo
-                logger.info("Il messaggio ricevuto non è un testo.")
-                return
+        if not update.message.text:  # Controlla se il messaggio non è testo
+            logger.info("Il messaggio ricevuto non è un testo.")
+            return
 
-            if update.message.reply_to_message:  # Controlla se il messaggio è una reply
-                logger.info("Il messaggio è una reply, nessuna azione intrapresa.")
-                return  # Non fa nulla se è una reply
-            else:
-                # Risponde nella chat con il messaggio desiderato
-                logger.info("Il messaggio NON è una reply, invio risposta...")
-                await update.message.reply_text("Questo messaggio non è una reply")
+        if update.message.reply_to_message:  # Controlla se il messaggio è una reply
+            logger.info("Il messaggio è una reply, nessuna azione intrapresa.")
+            return  # Non fa nulla se è una reply
+        else:
+            # Risponde nella chat con il messaggio desiderato
+            logger.info("Il messaggio NON è una reply, invio risposta...")
+            await update.message.reply_text("Questo messaggio non è una reply")
 
 # Funzione principale per avviare il bot
 def main():
