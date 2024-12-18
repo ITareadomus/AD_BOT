346396da8b3f838fc8ac9ab96e1d0ec7068349c5
Markk <marcoale.specchio@gmail.com>
2024-12-18 16:05:48 +0100

    log aggiunti

--- bot.py
+++ bot.py
@@ -11,17 +11,26 @@ TARGET_CHANNEL_ID = -1002350584252
 
 # Funzione per gestire i messaggi di testo
 async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    if update.message and update.message.chat.id == TARGET_CHANNEL_ID:  # Controlla se il messaggio è nel canale target
-        if update.message.reply_to_message:  # Controlla se il messaggio è una reply
-            return  # Non fa nulla se è una reply
-        else:
-            # Risponde nella chat con il messaggio desiderato
-            await update.message.reply_text("Questo messaggio non è una reply")
+    if update.message:
+        logger.info(f"Messaggio ricevuto nella chat con ID: {update.message.chat.id}")
+
+        if not update.message.text:  # Controlla se il messaggio non è testo
+            logger.info("Il messaggio ricevuto non è un testo.")
+            return
+
+        if update.message.chat.id == TARGET_CHANNEL_ID:  # Controlla se il messaggio è nel canale target
+            if update.message.reply_to_message:  # Controlla se il messaggio è una reply
+                logger.info("Il messaggio è una reply, nessuna azione intrapresa.")
+                return  # Non fa nulla se è una reply
+            else:
+                # Risponde nella chat con il messaggio desiderato
+                logger.info("Il messaggio NON è una reply, invio risposta...")
+                await update.message.reply_text("Questo messaggio non è una reply")
 
 # Funzione principale per avviare il bot
 def main():
     # Inserisci il tuo token fornito da BotFather
-    BOT_TOKEN = "7730646498:AAEvHUQjZSc_5OHoXiCwm64SDceyBEJO2go"
+    BOT_TOKEN = "INSERISCI_IL_TUO_TOKEN_QUI"
 
     # Crea l'applicazione del bot
     app = ApplicationBuilder().token(BOT_TOKEN).build()
